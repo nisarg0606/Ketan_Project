6c45bebe8fe3e3d42eca633bdcc3b480af99be68
nisarg001 <106367579+nshah-capri@users.noreply.github.com>
2023-03-31 18:06:01 +0530

    updated login api

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,16 +1,26 @@
 const User = require("../models/userSchema");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!user) {
       res.status(401).json({ message: "Invalid email}" });
-    } else if (await bcrypt.compare(password, user.password)) {
+    } else if (!isMatch) {
       res.status(401).json({ message: "Invalid password" });
     } else {
-      const token = await user.generateAuthToken();
-      res.status(200).json({ user, token });
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+      });
+      let userObj = user.toObject();
+      userObj.token = token;
+      delete userObj.password;
+      res.status(200).json(userObj);
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
