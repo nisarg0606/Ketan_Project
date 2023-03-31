b65d6e6d393f205fb10ffcae8d9b55a28b0a83d0
nisarg001 <106367579+nshah-capri@users.noreply.github.com>
2023-03-31 17:48:55 +0530

    Completed user CRUD

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -30,3 +30,53 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      await user.save();
+      res.status(200).json({ message: "User updated successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.remove();
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
