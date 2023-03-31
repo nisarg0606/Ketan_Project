0085c30fbd3d5abc88b0c70220d02c2ac0781946
nisarg001 <106367579+nshah-capri@users.noreply.github.com>
2023-03-31 17:49:31 +0530

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,9 @@ const { login, register } = require("../controllers/userContoller");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/", auth, userController.getUsers);
+router.get("/:id", auth, userController.getUser);
+router.put("/:id", auth, userController.updateUser);
+router.delete("/:id", auth, userController.deleteUser);
 
 module.exports = router;
